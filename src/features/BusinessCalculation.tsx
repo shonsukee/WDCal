60581220d3327c135d1c44328086d6619d7be308
inoueshoei <inoueshoei.313.9720@gmail.com>
2024-11-17 11:59:22 +0900

    [Add]休業日機能追加

--- src/features/BusinessCalculation.tsx
+++ src/features/BusinessCalculation.tsx
@@ -1,63 +1,130 @@
 "use client"
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Calendar } from "@/components/ui/calendar"
+import { Button } from "@/components/ui/button"
 import { ja } from "date-fns/locale"
-import { WorkingDateCalculation } from "@/features/WorkingDateCalculation"
+import { WorkingDateCalculation, isSameDay } from "@/features/WorkingDateCalculation"
 
 export const BusinessCalculation = () => {
-	const [date, setDate] = useState<Date | undefined>(new Date());
-	const [daysToAdd, setDaysToAdd] = useState<number | undefined>(undefined);
+	const [baseDate, setBaseDate] = useState<Date | undefined>(new Date());
+	const [workingDate, setWorkingDate] = useState<number | undefined>(undefined);	// 営業日
+	const [highlightedDates, setHighlightedDates] = useState<Date[]>([]);			// 休業日
+	const [deliveryDate, setDeliveryDate] = useState<Date | undefined>(undefined);	// 納期
+	const [isHolidayEditMode, setIsHolidayEditMode] = useState<Boolean>(false);		// 休業日の修正フラグ
 
-	const handleSetDate = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const days = parseInt(e.target.value);
-		if (!isNaN(days)) {
-			setDaysToAdd(days);
+	// 基準日を設定
+	const handleDirectSetDate = (selectedDate: Date | undefined) => {
+		setBaseDate(selectedDate);
+	};
 
-			// 営業日の計算
-			const workingDays: Date = WorkingDateCalculation(days);
-			setDate(workingDays);
-		} else {
-			setDaysToAdd(undefined);
+	// 営業日を設定
+	const handleSetDate = (selectedDate: number | undefined) => {
+		if (selectedDate === undefined) {
+			setWorkingDate(undefined);
+		} else if (!isNaN(selectedDate)) {
+			setWorkingDate(selectedDate);
 		}
 	};
+
+	// 休業日を設定
+	const handleSetBusinessHoliday = (selectedDate: Date | undefined) => {
+		if (!selectedDate) return;
+
+		// 休業日の追加
+		if (!highlightedDates.some((date) => isSameDay(date, selectedDate))) {
+			setHighlightedDates([selectedDate, ...highlightedDates]);
+
+		// 休業日の削除
+		} else {
+			const newDates = highlightedDates.filter((date) => !isSameDay(date, selectedDate));
+			setHighlightedDates(newDates);
+		}
+	}
+
+	// 納期を設定
+	useEffect(() => {
+		if (baseDate && workingDate !== undefined) {
+			const workingDays: Date = WorkingDateCalculation(baseDate, workingDate, highlightedDates);
+			setDeliveryDate(workingDays);
+		}
+	}, [baseDate, workingDate, highlightedDates]);
 
 	return (
 		<div className="flex flex-col items-center min-h-screen bg-gray-50 p-4">
 			<div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
-				<h1 className="text-3xl font-semibold text-center mb-8 text-gray-800">
+				<h1 className="text-3xl font-semibold text-center mb-8 text-gray-700">
 					営業日計算ツール
 				</h1>
 				<div className="flex flex-col space-y-6 items-center">
-					<div className="flex flex-col items-center">
-						<Label htmlFor="date" className="text-gray-700 mb-1 block text-left">
-							営業日
-						</Label>
-						<Input
-							id="date"
-							type="number"
-							min="0"
-							placeholder="例：3"
-							className="w-2/4 text-center"
-							onChange={handleSetDate}
-						/>
+					<div className="w-full h-full flex flex-row gap-4">
+						{/* 休業日 */}
+						<div className="flex flex-col justify-center items-center w-full p-4 border rounded-md shadow-sm bg-white">
+							<Label
+								htmlFor="business-holiday"
+								className="text-gray-700 mb-2 text-lg font-semibold"
+							>
+								休業日
+							</Label>
+							<Button
+								variant="outline"
+								onClick={() => setIsHolidayEditMode(!isHolidayEditMode)}
+								className={`px-4 py-2 rounded-md ${
+									isHolidayEditMode ? "bg-amber-500 hover:bg-amber-300 text-white" : ""
+								}`}
+							>
+								{isHolidayEditMode ? "終了" : "追加"}
+							</Button>
+						</div>
+
+						{/* 営業日 */}
+						<div className="flex flex-col justify-center items-center w-full p-4 border rounded-md shadow-sm bg-white">
+							<Label
+								htmlFor="date"
+								className="text-gray-700 mb-2 text-lg font-semibold"
+							>
+								営業日
+							</Label>
+							<Input
+								id="date"
+								type="number"
+								min="0"
+								placeholder="例：3"
+								className="w-3/4 text-center border border-gray-300 rounded-md p-2"
+								onChange={(e) => handleSetDate(parseInt(e.target.value))}
+							/>
+						</div>
 					</div>
 					<div className="flex flex-col items-center">
-						<Label className="text-gray-700 mb-2 text-center">カレンダーから日付を選択</Label>
+						<Label className="text-gray-700 mb-2 text-center">カレンダーから基準日を選択</Label>
 						<Calendar
 							locale={ja}
 							mode="single"
-							selected={date}
-							onSelect={(selectedDate) => setDate(selectedDate)}
+							selected={baseDate}
+							onSelect={isHolidayEditMode ? handleSetBusinessHoliday : handleDirectSetDate}
 							className="rounded-md border max-w-sm w-fit"
+							modifiers={{
+								highlighted: highlightedDates,
+							}}
+							modifiersStyles={{
+								highlighted: { backgroundColor: '#90cdf4' }
+							}}
 						/>
+						<div className="flex items-center mt-5">
+							<span className="block w-4 h-4 bg-black rounded-sm"></span>
+							<span className="ml-2 mr-10 text-gray-700 text-sm">基準日</span>
+							<span className="block w-4 h-4 bg-sky-300 rounded-sm"></span>
+							<span className="ml-2 text-gray-700 text-sm">休業日</span>
+						</div>
 					</div>
-					{daysToAdd !== undefined && (
-						<p className="text-center text-gray-700 mt-4">
-							{daysToAdd} 営業日後は <span className='font-bold'>{date?.toLocaleDateString()}</span> です。
-						</p>
+					{workingDate !== undefined && baseDate !== undefined && (
+						<>
+							<p className="text-center text-gray-700 mt-4">
+								{baseDate?.toLocaleDateString()}から<br/>{workingDate} 営業日後の<span className='font-bold'>納期</span>は<br/><span className='font-bold text-2xl'>{deliveryDate?.toLocaleDateString()}</span> です。
+							</p>
+						</>
 					)}
 				</div>
 			</div>
--- src/features/WorkingDateCalculation.ts
+++ src/features/WorkingDateCalculation.ts
@@ -14,29 +14,47 @@ function isHoliday(date: Date): boolean {
 }
 
 /**
+ * 同日かどうかを判定
+ * @param date1 休業日
+ * @param date2 基準日
+ * @returns 同日ならtrue、それ以外はfalse
+ */
+export function isSameDay(date1: Date, date2: Date): boolean {
+	return date1.getDate() === date2.getDate() &&
+	date1.getMonth() === date2.getMonth() &&
+	date1.getFullYear() === date2.getFullYear();
+}
+
+/**
  * 指定された日付が営業日かどうかを判定
- * 営業日は平日（月曜から金曜）かつ祝日でない日を指す
+ * 営業日は平日（月曜から金曜）かつ祝日および休業日でない日を指す
  * @param date 判定する日付
+ * @param holiday 休業日
  * @returns 営業日の場合はtrue、それ以外はfalse
  */
-function isBusinessDay(date: Date): boolean {
-	return BUSINESS_DAYS.includes(date.getDay()) && !isHoliday(date);
+function isBusinessDay(date: Date, holiday: Date[]): boolean {
+	return BUSINESS_DAYS.includes(date.getDay()) &&
+	!isHoliday(date) &&
+	!holiday.some((business_day) => isSameDay(business_day, date));
 }
+
 /**
  * 指定された日付から指定された営業日数後の日付を取得
- * 土日と祝日はスキップ
- * @param date 基準となる日付
- * @param days 追加する営業日数
+ * 土日と祝日と休業日はスキップ
+ * @param date 基準日
+ * @param days 営業日数
+ * @param holiday 休業日
  * @returns 指定された営業日数後の日付
  */
-function addBusinessDays(date: Date, days: number): Date {
+function addBusinessDays(date: Date, days: number, holiday: Date[]): Date {
 	let result = new Date(date);
 	let remainingDays = days;
 	const direction = days >= 0 ? 1 : -1;
 
 	while (remainingDays !== 0) {
 	  result = addDays(result, direction);
-	  if (isBusinessDay(result)) {
+	  // 営業日のみデクリメント
+	  if (isBusinessDay(result, holiday)) {
 		remainingDays -= direction;
 	  }
 	}
@@ -45,11 +63,13 @@ function addBusinessDays(date: Date, days: number): Date {
 }
 
 /**
- * 指定された日数後の営業日を取得
+ * 指定された営業日後の納期を取得
+ * @param date 基準日
  * @param addDays 追加する日数
- * @returns 営業日
+ * @param holiday 休業日
+ * @returns 納期
  */
-export const WorkingDateCalculation = (addDays: number) => {
-	const workingDate: Date = addBusinessDays(new Date(), addDays);
+export const WorkingDateCalculation = (date: Date, addDays: number, holiday: Date[]) => {
+	const workingDate: Date = addBusinessDays(date, addDays+1, holiday);
 	return workingDate;
 }
